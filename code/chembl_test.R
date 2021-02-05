df4baea53f871429d2c701ea437a1c906e8aaeab
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2021-02-05 19:08:27 +0100

    Actualización chembl_test.R

--- code/chembl_test.R
+++ code/chembl_test.R
@@ -1,4 +1,4 @@
-# Este script se ha obtenido a partir de un c√≥digo encontrado en https://gist.github.com/mnowotka/99a232900116df22be84ead82f234d9e.
+# Este script se ha obtenido a partir de un c√É¬≥digo encontrado en https://gist.github.com/mnowotka/99a232900116df22be84ead82f234d9e.
 
 
 
@@ -6,16 +6,19 @@
 #################### LIBRERIAS ##################################
 #################################################################
 
+
+dir <- getwd()
+libr <- paste(dir,"/software/deps", sep = "")
+.libPaths(libr)
+
+
 #Las librerias se tienen que instalar en la carpeta SOFTWARE
 library(jsonlite) ## Tiene que ser la ultima version (1.7.2)
 library(httr)
-# install.packages('devtools')
 library(devtools)
-# devtools::install_github('rajarshi/chemblr/package')
 library(chemblr)
 library(sjmisc)
 library(tidyverse)
-# install.packages(""networkD3"")
 library(networkD3)
 library(magrittr)
 
@@ -65,12 +68,12 @@ get_objects2 <- function(url){
 }
 
 
-# Obtiene los ids de todos los f·rmacos de una fase indicada y guarda en una lista toda la info de los f·rmacos 
+# Obtiene los ids de todos los f√°rmacos de una fase indicada y guarda en una lista toda la info de los f√°rmacos 
 get_all_drugs <- function(fase_farmaco){
   
-  limite_farmacos <- 1000 #N˙mero de f·rmacos por p·gina
-  fase_farmacos <- fase_farmaco #Fase en el que se encuentra un f·rmaco (en fase 4 ya est· aprobado)
-  offset_farmacos <- 0 #PosiciÛn del f·rmaco que se muestra el primero en la p·gina (pasar p·gina)
+  limite_farmacos <- 1000 #N√∫mero de f√°rmacos por p√°gina
+  fase_farmacos <- fase_farmaco #Fase en el que se encuentra un f√°rmaco (en fase 4 ya est√° aprobado)
+  offset_farmacos <- 0 #Posici√≥n del f√°rmaco que se muestra el primero en la p√°gina (pasar p√°gina)
   
   URL_farmacos <- file.path(URL_ROOT, 
                             paste("molecule?limit=",limite_farmacos,
@@ -91,7 +94,7 @@ get_all_drugs <- function(fase_farmaco){
   # Numero de paginas si mostramos con pagina 1000
   num_paginas <- ceiling(num_total_farmacos/limite_farmacos)
   
-  #Si hay m·s de una p·gina, hacemos el proceso anterior con las que haya
+  #Si hay m√°s de una p√°gina, hacemos el proceso anterior con las que haya
   for (pagina in 1:(num_paginas-1)){
     offset_farmacos <- offset_farmacos + 1000
     URL_farmacos <- file.path(URL_ROOT, 
@@ -108,12 +111,12 @@ get_all_drugs <- function(fase_farmaco){
     vector_farmacos_id <- c(vector_farmacos_id, vector_farmacos_id_intermedio)
   }
   
-  # Devolvemos un vector con todos los IDs de los f·rmacos
+  # Devolvemos un vector con todos los IDs de los f√°rmacos
   return(list("vector_farmacos_ids" = vector_farmacos_id, "info_farmacos" = lista_info_farmacos))
 }
 
 
-# Pasandole una pagina con f·rmacos, devuelve un vector con los ids de los farmacos
+# Pasandole una pagina con f√°rmacos, devuelve un vector con los ids de los farmacos
 get_drugs_from_page <- function(farmacos_page){
   vector_farmacos_id <- vector()
   for (posicion in 1:length(farmacos_page$molecules)){
@@ -126,14 +129,14 @@ get_drugs_from_page <- function(farmacos_page){
 
 
 # Le pasamos el vector con los ids de las drogas y las proteinas targets que nos interesa
-# Devuelve una lista donde el nombre corresponde al target y el contenido ser· un vector con los f·rmacos asociados
+# Devuelve una lista donde el nombre corresponde al target y el contenido ser√° un vector con los f√°rmacos asociados
 # Devuelve un dataframe con las drogas seleccionadas y la informacion relacionada
 get_targets <- function(drug_vector, proteinas_targets, drug_info){
   #Para devolver: lista y dataframe
   lista_target_drug <- list()
   informacion_farmacos <- data.frame("id_farmaco" = NA,
                                      "id_target" = NA, 
-                                     "fecha_aprobaciÛn" = NA,
+                                     "fecha_aprobaci√≥n" = NA,
                                      "canonical_smile" = NA, 
                                      "action_type" = NA,
                                      "mechanism_of_action" = NA)
@@ -158,7 +161,7 @@ get_targets <- function(drug_vector, proteinas_targets, drug_info){
       print("Se ha alcanzado un 75% de la carga")
     }
    ################################################ 
-    # Hacemos peticiÛn para traernos el mecanismo y conseguir los ids de los target (si tiene alguno)
+    # Hacemos petici√≥n para traernos el mecanismo y conseguir los ids de los target (si tiene alguno)
     id_farmaco <- drug_vector[posDrug]
     URL_targets <- file.path(URL_ROOT, 
                              paste("mechanism?molecule_chembl_id=", 
@@ -202,32 +205,32 @@ get_targets <- function(drug_vector, proteinas_targets, drug_info){
 }
 
 # Cargamos los datos que queremos de las drogas
-# AÒade al dataframe la info de un f·rmaco. Esta informaciÛn puede venir de la consulta de todos los f·rmacos o de la consulta de los mecanismos
+# A√±ade al dataframe la info de un f√°rmaco. Esta informaci√≥n puede venir de la consulta de todos los f√°rmacos o de la consulta de los mecanismos
 get_drug_info <- function(tablaInfo, drugID, drugPos, drugInfo, mechanism, targetpos, targetID){
   
-  ######## Info del listado de f·rmacos ###########
+  ######## Info del listado de f√°rmacos ###########
   pagina <- ceiling(drugPos/1000)
   posicion_en_pagina <- (drugPos-((pagina-1)*1000))
   farmaco_info <- drugInfo[[pagina]]$molecules[[posicion_en_pagina]]
   
-  # Fecha de aprobaciÛn del f·rmaco
+  # Fecha de aprobaci√≥n del f√°rmaco
   fecha_aprobacion <- farmaco_info$first_approval
   # Canonical smile
   c_smile <- farmaco_info$molecule_structures[1]
   
-  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Mirar para aÒadir m·s !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Mirar para a√±adir m√°s !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
   ######## Info del mecanismo ###########
   # Tipo de accion
   action_type <- mechanism$mechanisms[[targetpos]]$action_type
-  # Mecanismo de acciÛn
+  # Mecanismo de acci√≥n
   mechanism_of_action <- mechanism$mechanisms[[targetpos]]$mechanism_of_action
   
-  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Mirar para aÒadir m·s !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Mirar para a√±adir m√°s !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
   tablaInfo <- rbind(tablaInfo, c("id_farmaco" = drugID,
                                   "id_target" = targetID, 
-                                  "fecha_aprobaciÛn" = fecha_aprobacion,
+                                  "fecha_aprobaci√≥n" = fecha_aprobacion,
                                   "canonical_smile" = c_smile,
                                   "action_type" = action_type,
                                   "mechanism_of_action" = mechanism_of_action))
@@ -243,7 +246,7 @@ get_drug_info <- function(tablaInfo, drugID, drugPos, drugInfo, mechanism, targe
 ################### EJECUCION ###################################
 #################################################################
 
-# Todos los f·rmacos en fase 4. Tanto para proteinas de 1 y 2 grado
+# Todos los f√°rmacos en fase 4. Tanto para proteinas de 1 y 2 grado
 
 farmacos_fase4 <- get_all_drugs(fase_farmaco = 4)
 farmacos_ids_fase4 <- farmacos_fase4[["vector_farmacos_ids"]]
@@ -252,7 +255,7 @@ farmacos_info_fase4 <- farmacos_fase4[["info_farmacos"]]
 targets_proteinas_1g <- get_targets(drug_vector = farmacos_ids_fase4, proteinas_targets = proteinas_targets, drug_info = farmacos_info_fase4)
 targets_proteinas_2g <- get_targets(drug_vector = farmacos_ids_fase4, proteinas_targets = proteinas_targets_segundo_grado, drug_info = farmacos_info_fase4)
 
-# Todos los f·rmacos en fase 3. Tanto para proteinas de 1 y 2 grado
+# Todos los f√°rmacos en fase 3. Tanto para proteinas de 1 y 2 grado
 
 farmacos_fase3 <- get_all_drugs(fase_farmaco = 3)
 farmacos_ids_fase3 <- farmacos_fase3[["vector_farmacos_ids"]]
@@ -268,9 +271,9 @@ targets_proteinas_2g_f3 <- get_targets(drug_vector = farmacos_ids_fase3, protein
 ################### Pruebas(borrar) #############################
 #################################################################
 
-limite_farmacos <- 1000 #N˙mero de f·rmacos por p·gina
-fase_farmacos <- fase_farmaco #Fase en el que se encuentra un f·rmaco (en fase 4 ya est· aprobado)
-offset_farmacos <- 0 #PosiciÛn del f·rmaco que se muestra el primero en la p·gina (pasar p·gina)
+limite_farmacos <- 1000 #N√∫mero de f√°rmacos por p√°gina
+fase_farmacos <- fase_farmaco #Fase en el que se encuentra un f√°rmaco (en fase 4 ya est√° aprobado)
+offset_farmacos <- 0 #Posici√≥n del f√°rmaco que se muestra el primero en la p√°gina (pasar p√°gina)
 
 URL_farmacos <- file.path(URL_ROOT, 
                           paste("molecule?limit=",limite_farmacos,
@@ -350,7 +353,7 @@ view_Action_Type <- function(type_target){
   return(grapic)
 }
 
-#para guardar las gr·ficas realizadas
+#para guardar las gr√°ficas realizadas
 #tipos de activadores
 jpeg(file="action_protein_1g.jpeg")
 action_protein_1g <- view_Action_Type(targets_proteinas_1g$df_informacion_farmacos$action_type)
@@ -362,7 +365,7 @@ jpeg(file="action_protein_2g_f3.jpeg")
 action_protein_2g_f3 <- view_Action_Type(targets_proteinas_2g_f3$df_informacion_farmacos$action_type)
 dev.off()
 
-#mechanismos de acciÛn
+#mechanismos de acci√≥n
 jpeg(file="mechanism_protein_1g.jpeg")
 mechanism_protein_1g <- view_Action_Type(targets_proteinas_1g$df_informacion_farmacos$mechanism_of_action)
 jpeg(file="mechanism_protein_2g.jpeg")
@@ -375,7 +378,7 @@ dev.off()
 
 
 
-#red de interacciÛn
+#red de interacci√≥n
 
 view_Interaction <- function(comp, type, drug){
   proteins <- c()
