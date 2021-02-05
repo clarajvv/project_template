86e5af4231cf1e09143b2ff017eb16c2950f7154
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2021-02-05 18:58:21 +0100

    Update setup.sh

--- code/setup.sh
+++ code/setup.sh
@@ -4,7 +4,15 @@ chmod 755 software/deps
 
 paquetesCRAN="'jsonlite' 'httr' 'devtools' 'sjmisc' 'tidyverse' 'networkD3' 'magrittr'"
 
+paquetesDevtools="'rajarshi/chemblr/package'"
+
 for paqueteCRAN in $paquetesCRAN
 	do 
 		Rscript -e 'install.packages('$paqueteCRAN', repos="https://cran.rstudio.com/", lib = "software/deps" )'
 	done 
+
+for paqueteDevtools in $paquetesDevtools
+	do 
+		Rscript -e 'devtools::install_github('$paqueteDevtools', lib = "software/deps" )'
+	done
+	
